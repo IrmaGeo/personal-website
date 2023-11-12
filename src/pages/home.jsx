5549e6a4f4d289af9d245fa9e1aece2c58f4fe6a
IrmaGeo <irmamodzg@gmail.com>
2023-11-11 22:40:27 -0500

    changed home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,81 +1,73 @@
-// Home.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Use free-brands-svg-icons for social icons
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import "../styles/styles.css";
 
-const Home = () => (
-  console.log("REACT_APP_PUBLIC_URL:", process.env.REACT_APP_PUBLIC_URL);
+const Home = () => {
+  return (
+    <body className='flex-container'>
+      <div className='profile-info'>
+        {/* Profile Image */}
+        <img src="https://irmageo.github.io/personal-website/resources/images/profile.jpg" alt='Irma Modzgvrishvili' />
 
-   <body className='flex-container'>
-  <div className='profile-info'>
-  {/* Profile Image */}
-  <img src="https://irmageo.github.io/personal-website/resources/images/profile.jpg" alt='Irma Modzgvrishvili'/>
-  
+        {/* Name */}
+        <h1>Irma Modzgvrishvili</h1>
 
-  {/* Name */}
-  <h1>Irma Modzgvrishvili</h1>
+        {/* Occupation or Title */}
+        <p>Machine Learning Explorer | AI Visionary | Data Artisan</p>
 
-  {/* Occupation or Title */}
-  <p>Machine Learning Explorer | AI Visionary | Data Artisan</p>
+        {/* Email Link */}
+        <i className="fa fa-hand-o-down fa-4x" aria-hidden="true"></i>
+        <br />
+        <br />
+        <a href="mailto:modzgvrishviliirma@email.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
 
-  {/* Email Link */}
- 
- <i class="fa fa-hand-o-down fa-4x" aria-hidden="true"></i>
- <br />
- <br />
-  <a href="mailto:modzgvrishviliirma@email.com">
-    <FontAwesomeIcon icon={faEnvelope} />
-  </a>
+        {/* LinkedIn Link */}
+        <a href="https://www.linkedin.com/in/irmamodzgvrishvili" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
 
-  {/* LinkedIn Link */}
-  <a href="https://www.linkedin.com/in/irmamodzgvrishvili" target="_blank" rel="noopener noreferrer">
-    <FontAwesomeIcon icon={faLinkedin} />
-  </a>
+        {/* GitHub Link */}
+        <a href="https://github.com/IrmaGeo" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
 
-  {/* GitHub Link */}
-  <a href="https://github.com/IrmaGeo" target="_blank" rel="noopener noreferrer">
-    <FontAwesomeIcon icon={faGithub} />
-  </a>
+      <div className='data-description'>
+        <h1>About Me</h1>
+        {/* Introduction: Describes the role and skills */}
+        <p>
+          Welcome! I'm Irma, a data magician skilled in the art of transforming raw data into captivating stories and actionable insights. 📊
+        </p>
 
-</div>
+        {/* Greeting and mission statement */}
+        <p>
+          My journey through the ever-expanding data cosmos has equipped me with the superpowers to analyze, visualize, and unearth valuable treasures from complex datasets. With a passion for unraveling data's hidden secrets, I'm here to weave wonders with information just for you!
+        </p>
 
+        {/* Skills and purpose */}
+        <p>
+          Beyond my data adventures, I'm an avid explorer of the AI and machine learning realms. I stay on the cutting edge, continuously learning and adapting to the latest trends. My education includes a degree in Data Science from Data University, where I honed my craft and ignited my passion for the world of numbers and patterns.
+        </p>
 
-<div className='data-description'>
-  <h1>About Me</h1>
-  {/* Introduction: Describes the role and skills */}
-  <p>
-  Welcome! I'm Irma, a data magician skilled in the art of transforming raw data into captivating stories and actionable insights. 📊  </p>
-  
-  {/* Greeting and mission statement */}
-  <p>
-  My journey through the ever-expanding data cosmos has equipped me with the superpowers to analyze, visualize, and unearth valuable treasures from complex datasets. With a passion for unraveling data's hidden secrets, I'm here to weave wonders with information just for you!
+        {/* Interests and call to action */}
+        <p>
+          Join me on this data-driven odyssey, and together, we'll shape a smarter, brighter future!
+        </p>
 
-</p>
-  
-  {/* Skills and purpose */}
-  <p>
-  Beyond my data adventures, I'm an avid explorer of the AI and machine learning realms. I stay on the cutting edge, continuously learning and adapting to the latest trends. My education includes a degree in Data Science from Data University, where I honed my craft and ignited my passion for the world of numbers and patterns.
-
-</p>
-  
-  {/* Interests and call to action */}
-  <p>
-  Join me on this data-driven odyssey, and together, we'll shape a smarter, brighter future!  </p>
-
-  <h2>Interest</h2>
-  <ul>
-    <li>Deep Learning</li>
-    <li>Machine Learning</li>
-    <li>Data Science</li>
-    <li>Artificial Intelligence</li>
-  </ul>
- 
-</div>
-
-</body>
-
-);
+        <h2>Interest</h2>
+        <ul>
+          <li>Deep Learning</li>
+          <li>Machine Learning</li>
+          <li>Data Science</li>
+          <li>Artificial Intelligence</li>
+        </ul>
+      </div>
+    </body>
+  );
+};
 
 export default Home;
