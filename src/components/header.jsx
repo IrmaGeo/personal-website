3004ca8636e07f527bef59ca9e935c0ac1f268c5
IrmaGeo <irmamodzg@gmail.com>
2023-11-11 21:14:03 -0500

    marges branches

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "../styles/styles.css";
+const Header = () => (
+  <header className="header">
+   
+    
+    <div className='test'>
+     
+        {/* Navigation Links */}
+        <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
+        <NavLink to="/projects" className="nav-link" activeClassName="active">Projects</NavLink>
+        <NavLink to="/resume" className="nav-link" activeClassName="active">Resume</NavLink>
+    
+    </div>
+  </header>
+);
+
+export default Header;
