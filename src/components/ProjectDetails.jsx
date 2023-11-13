4999f55e71903040afaca9f94846e880ee173cdc
IrmaGeo <irmamodzg@gmail.com>
2023-11-12 21:57:20 -0500

    change projectdetails file

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,27 +1,30 @@
-// ProjectDetails.js
-
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const ProjectDetails = ({ category, projects }) => (
-  <div className="category-container">
-    <h2>{category} Projects</h2>
-    <Carousel
-      showArrows
-      infiniteLoop
-      showThumbs={false}
-      autoPlay
-      interval={5000}
-      stopOnHover={false}
-      className="custom-carousel"
-    >
-      {projects.map((project) => (
-        <ProjectCard key={project.Id} project={project} />
-      ))}
-    </Carousel>
-  </div>
-);
+const ProjectDetails = ({ category, projects }) => {
+
+  return (
+    <div className="category-container">
+      <h2>{category} Projects</h2>
+      <Carousel
+        showArrows
+        infiniteLoop
+        showThumbs={false}
+        autoPlay
+        interval={5000}
+        stopOnHover={false}
+        className="custom-carousel"
+      >
+        {projects.map((project) => (
+          <div key={project.id}>
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default ProjectDetails;
