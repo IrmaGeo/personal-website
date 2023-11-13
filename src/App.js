f5059c2fd672d3dfc8ba33a7b79f7798291cbe2e
IrmaGeo <irmamodzg@gmail.com>
2023-11-13 00:40:06 -0500

    change projects name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
 import Footer from  "./components/footer";
 import Home from "./pages/home";
-import Projects from "./pages/projects"
+import Portfolio from "./pages/portfolio"
 
 import Resume from "./pages/resume";
 
@@ -17,7 +17,7 @@ import Resume from "./pages/resume";
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/resume" element={<Resume/>} />
-        <Route path="/projects" element={<Projects/>} />
+        <Route path="/projects" element={<Portfolio/>} />
 
         
       </Routes>
