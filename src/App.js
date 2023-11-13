6e7c9a307c776440f93193384d3c03a01b8dd746
IrmaGeo <irmamodzg@gmail.com>
2023-11-12 22:58:02 -0500

    remove photo from home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
 import Footer from  "./components/footer";
 import Home from "./pages/home";
-import Projects from './pages/projects';
+import Projects from "./pages/projects"
 
 import Resume from "./pages/resume";
 
@@ -15,9 +15,11 @@ import Resume from "./pages/resume";
     <div className="container">
       <Header />
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="projects" element={<Projects />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/resume" element={<Resume/>} />
+        <Route path="/projects" element={<Projects/>} />
+
+        
       </Routes>
       <Footer/>
       </div>
